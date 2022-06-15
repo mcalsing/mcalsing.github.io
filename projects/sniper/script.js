891cf4f0cb2f7847cdfb44f9a9e4885083d805ff
Marcelo Calsing <marcelocalsing@gmail.com>
2022-06-14 23:58:31 -0300

    Pedra descendo e box atirando so no meio

---
+++ projects/sniper/script.js
@@ -0,0 +1,39 @@
+const box = document.querySelector('.box');
+const rock = document.querySelector('.rock');
+const bullet = document.querySelector('.bullet');
+const gameBoard = document.querySelector('.game-board');
+
+let modifier = 5;
+
+window.addEventListener("keydown", (eventOrigin) => {
+    switch (eventOrigin.key) {
+        case 'Enter': box.style.left = `${250}px`; break;
+        case 'ArrowLeft': box.style.left = `${parseInt(box.style.left) - modifier}px`; break;
+        case 'ArrowRight': box.style.left = `${parseInt(box.style.left) + modifier}px`; break;
+    }
+});
+
+// Faz a verificacao da posicao da rock e da box, para aplicar colisao
+const loop = setInterval(() => {
+
+    const rockPosition = +window.getComputedStyle(rock).bottom.replace('px', '');
+    const boxPosition = box.offsetLeft;
+
+    if (rockPosition <= 60 && boxPosition <= 80) {
+        rock.style.animation = 'none';
+        rock.style.bottom = `${rockPosition}px`
+    }
+
+}, 20);
+
+function shotBullet() {
+     const newShot = document.createElement('div');
+     newShot.className = 'bullet shot';
+     gameBoard.appendChild(newShot);
+     setTimeout(() => {
+        document.querySelector('.shot').remove();
+     }, 900);
+}
+
+document.addEventListener('keyup', shotBullet);
+
