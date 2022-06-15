e5059e302ebce5e2cff5ff3034d529b11137bc1f
Marcelo Calsing <marcelocalsing@gmail.com>
2022-06-15 14:11:55 -0300

    Aplicado colisao do tira contra a rocha

--- projects/sniper/script.js
+++ projects/sniper/script.js
@@ -3,8 +3,10 @@ const rock = document.querySelector('.rock');
 const bullet = document.querySelector('.bullet');
 const gameBoard = document.querySelector('.game-board');
 
+// Modificador de velocidade de deslocamento horizontal.
 let modifier = 5;
 
+// Movimentacao horizontal do nave
 window.addEventListener("keydown", (eventOrigin) => {
     switch (eventOrigin.key) {
         case 'Enter': box.style.left = `${250}px`; break;
@@ -13,23 +15,41 @@ window.addEventListener("keydown", (eventOrigin) => {
     }
 });
 
-// Faz a verificacao da posicao da rock e da box, para aplicar colisao
+// Verifica a posicao da rock e da box a cada 100ms, para aplicar colisao
 const loop = setInterval(() => {
+    const shot = document.querySelector('.shot');
 
     const rockPosition = +window.getComputedStyle(rock).bottom.replace('px', '');
     const boxPosition = box.offsetLeft;
-
+    const shotPositionH = box.offsetLeft + 13;
+    console.log('Posicao horizontal: ', shotPositionH);
+    
+    // Verifica a posicao da pedra em relacao ao tiro, para aplicar colisao
+    if (shot !== null) {
+        const shotPositionV = +window.getComputedStyle(shot).bottom.replace('px', '');
+        
+        diferencaShotToRock = Math.abs(rockPosition - shotPositionV);
+        if (diferencaShotToRock <= 15 && shotPositionH < 80) {
+            rock.style.animation = 'none';
+            rock.style.bottom = `${rockPosition}px`
+            alert('You Win!')
+        }
+    }
+    
     if (rockPosition <= 60 && boxPosition <= 80) {
         rock.style.animation = 'none';
         rock.style.bottom = `${rockPosition}px`
+        alert('You Lose!')
     }
 
-}, 20);
+    
+
+}, 100);
 
 // Cria a div que funciona como um tiro saindo da nave.
 function shotBullet() {
      const newShot = document.createElement('div');
-     newShot.className = 'bullet shot';
+     newShot.className = 'shot bullet';
      box.appendChild(newShot);
      setTimeout(() => {
         document.querySelector('.shot').remove();
